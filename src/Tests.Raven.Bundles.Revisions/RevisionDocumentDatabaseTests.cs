9e4a481af9cc2bd12b9032667e02bc8bd6b32256
thefringeninja <joao.paul@braganca.name>
2016-05-10 10:51:04 +0200

    updated to a more recent version of raven

--- src/Tests.Raven.Bundles.Revisions/RevisionDocumentDatabaseTests.cs
+++ src/Tests.Raven.Bundles.Revisions/RevisionDocumentDatabaseTests.cs
@@ -20,6 +20,8 @@
 //  --------------------------------------------------------------------------------------------------------------------
 #endregion
 
+using Raven.Abstractions.Exceptions;
+
 namespace Tests.Raven.Bundles.Revisions
 {
 	using System;
@@ -28,7 +30,6 @@ namespace Tests.Raven.Bundles.Revisions
 	using global::Raven.Bundles.Revisions;
 	using global::Raven.Client.Embedded;
 	using global::Raven.Client.Revisions;
-	using global::Raven.Database.Exceptions;
 
 	public class RevisionDocumentDatabaseTests : IDisposable
 	{
